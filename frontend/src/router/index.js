115dc43d17fac0fc2b0f984c75e48afc45ba9d35
Julien Richoz <julienrichoz@outlook.com>
2024-10-05 11:12:29 +0200

    Cleanup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,9 +8,9 @@ import HomePage from '../views/HomePage.vue';
 
 
 const routes = [
-  { 
-    path: '/:pathMatch(.*)*', 
-    name: 'NotFound', 
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
     component: () => import('../views/NotFound.vue')  // Crée une vue NotFound.vue
   },
   { path: '/', name: 'Home', component: HomePage },
@@ -26,7 +26,7 @@ const routes = [
     path: '/user',
     name: 'User',
     component: UserPage,
-    meta: { requiresAuth: true}
+    meta: { requiresAuth: true }
   },
 ];
 
@@ -36,16 +36,16 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    const isAuthenticated = store.state.isAuthenticated; // Vérifie si l'utilisateur est authentifié
-    const userRole = store.state.user?.roleId; // Récupère le rôle de l'utilisateur
-  
-    if (to.meta.requiresAuth && !isAuthenticated) {
-      next('/login'); // Redirige vers login si l'utilisateur n'est pas authentifié
-    } else if (to.meta.role && to.meta.role !== userRole) {
-      next('/'); // Redirige vers la page d'accueil si l'utilisateur n'a pas le bon rôle
-    } else {
-      next(); // Passe à la route suivante
-    }
-  });
+  const isAuthenticated = store.state.isAuthenticated; // Vérifie si l'utilisateur est authentifié
+  const userRole = store.state.user?.roleId; // Récupère le rôle de l'utilisateur
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login'); // Redirige vers login si l'utilisateur n'est pas authentifié
+  } else if (to.meta.role && to.meta.role !== userRole) {
+    next('/'); // Redirige vers la page d'accueil si l'utilisateur n'a pas le bon rôle
+  } else {
+    next(); // Passe à la route suivante
+  }
+});
 
 export default router;
