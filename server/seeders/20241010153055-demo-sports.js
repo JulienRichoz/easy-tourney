e7f7462fbf325e10130173eb5aea235ab42498e2
Julien Richoz <julienrichoz@outlook.com>
2024-10-10 17:35:48 +0200

    Add seeders with images

---
+++ server/seeders/20241010153055-demo-sports.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Sports', [
+      {
+        name: 'Basketball',
+        rule: 'Règles de basketball...',
+        scoreSystem: 'ASC',
+        color: '#FF5733',
+        image: '/uploads/basketball.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Football',
+        rule: 'Règles de football...',
+        scoreSystem: 'ASC',
+        color: '#33FF57',
+        image: '/uploads/football.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Volleyball',
+        rule: 'Règles de volleyball...',
+        scoreSystem: 'ASC',
+        color: '#FFC300',
+        image: '/uploads/volleyball.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Ultimate',
+        rule: 'Règles de ultimate...',
+        scoreSystem: 'ASC',
+        color: '#8E44AD',
+        image: '/uploads/ultimate.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Badminton',
+        rule: 'Règles de badminton...',
+        scoreSystem: 'ASC',
+        color: '#33A1FF',
+        image: '/uploads/badminton.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Sports', null, {});
+  },
+};
