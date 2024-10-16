e131ea10de130ca2e10f59eb5e332c48f8c790ed
Julien Richoz <julienrichoz@outlook.com>
2024-10-16 12:41:49 +0200

    Reassign non-testing value for limiter and jwt expiration

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -33,8 +33,8 @@ const errorHandler = (err, req, res, next) => {
 
 // Limiteur de requêtes pour limiter le nombre de requêtes d'une même IP
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 200, // Limite chaque IP à 200 requêtes par fenêtre de 15 minutes
+    windowMs: 1 * 60 * 1000, // 15 minutes
+    max: 2000, // Limite chaque IP à 200 requêtes par fenêtre de 15 minutes
     message: 'Trop de requêtes depuis cette IP, veuillez réessayer plus tard.',
 });
 
