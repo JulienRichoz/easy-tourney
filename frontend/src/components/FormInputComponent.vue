76911758d366965a6fecc0a07424c27facd8ca25
Julien Richoz <julienrichoz@outlook.com>
2024-10-18 10:44:00 +0200

    Rework FormInputComponent

--- frontend/src/components/FormInputComponent.vue
+++ frontend/src/components/FormInputComponent.vue
@@ -3,30 +3,19 @@
     <label :for="id" class="block text-gray-700 font-semibold mb-2">{{
       label
     }}</label>
-    <div v-if="type === 'color'" class="flex items-center">
-      <input
-        :type="type"
-        :id="id"
-        :value="modelValue"
-        @input="updateValue"
-        class="w-16 h-16 p-2 border border-gray-300 rounded-md mr-4"
-      />
-      <span class="text-gray-700 font-semibold">{{ modelValue }}</span>
-      <!-- Affichage de la valeur hexadécimale -->
-    </div>
     <input
-      v-else
       :type="type"
       :id="id"
       :placeholder="placeholder"
       :value="modelValue"
       @input="updateValue"
+      @blur="$emit('blur')"
       :class="[
         'w-full p-2 border rounded-md',
-        error ? 'border-red-500' : 'border-gray-300',
+        error && touched ? 'border-red-500' : 'border-gray-300',
       ]"
     />
-    <p v-if="error" class="text-red-500 text-sm mt-1">{{ error }}</p>
+    <p v-if="error && touched" class="text-red-500 text-sm mt-1">{{ error }}</p>
   </div>
 </template>
 
@@ -53,14 +42,49 @@
         type: String,
         default: '',
       },
-      error: {
-        type: String,
-        default: null,
+      required: {
+        type: Boolean,
+        default: false,
+      },
+      validate: {
+        type: Boolean,
+        default: false,
+      },
+      touched: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    data() {
+      return {
+        error: null,
+      };
+    },
+    watch: {
+      modelValue: {
+        handler(value) {
+          if (this.required && !value) {
+            this.error = 'Ce champ est requis';
+          } else if (
+            this.validate &&
+            this.type === 'email' &&
+            !this.isValidEmail(value)
+          ) {
+            this.error = 'Veuillez entrer une adresse email valide';
+          } else {
+            this.error = null;
+          }
+        },
+        immediate: true,
       },
     },
     methods: {
       updateValue(event) {
         this.$emit('update:modelValue', event.target.value);
+      },
+      isValidEmail(email) {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
       },
     },
   };
