c51e8597ac6cd798ee3ee25758aff93344b4e8a8
Julien Richoz <julienrichoz@outlook.com>
2024-10-25 20:37:26 +0200

    Add fa-users icon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -26,13 +26,14 @@ import {
     faPeopleGroup,
     faTrash,
     faPen,
+    faUsers
 } from '@fortawesome/free-solid-svg-icons'
 import './assets/tailwind.css'; // Import Tailwind CSS
 import 'leaflet/dist/leaflet.css';
 import './registerServiceWorker'
 
 // Ajouter les icônes supplémentaires
-library.add(faPen, faTrash, faPeopleGroup, faCog, faQuestionCircle, faMapMarkerAlt, faCalendar, faCalendarAlt, faSun, faMoon, faUser, faPowerOff, faSignInAlt, faInfoCircle, faMap, faFutbol, faFilter)
+library.add(faUsers, faPen, faTrash, faPeopleGroup, faCog, faQuestionCircle, faMapMarkerAlt, faCalendar, faCalendarAlt, faSun, faMoon, faUser, faPowerOff, faSignInAlt, faInfoCircle, faMap, faFutbol, faFilter)
 
 const app = createApp(App);
 
