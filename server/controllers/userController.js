9f299f451ee9ee35868334eedff692cb06cf6578
Julien Richoz <julienrichoz@outlook.com>
2024-11-04 11:45:36 +0100

    Add custom routes to userController. Limit exposivity of api response

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 // server/controllers/userController.js
 const { User, Tourney, Role, UsersTourneys, Team } = require('../models');
 const { roles } = require('../config/roles');
+const bcrypt = require('bcrypt');
 
 // Récupérer le profil de l'utilisateur authentifié
 exports.getOwnProfile = async (req, res) => {
@@ -40,11 +41,138 @@ exports.getOwnProfile = async (req, res) => {
     }
 };
 
+// Récupérer tous les utilisateurs avec leurs tournois et équipes (admin seulement)
+exports.getAllUsersWithDetails = async (req, res) => {
+    try {
+        // Vérifier que l'utilisateur est admin
+        if (req.user.roleId !== roles.ADMIN) {
+            return res.status(403).json({ message: 'Accès interdit.' });
+        }
+
+        const { tourneyId } = req.query;
+
+        const whereCondition = {};
+        if (tourneyId) {
+            whereCondition['$usersTourneys.tourneyId$'] = tourneyId;
+        }
+
+        const users = await User.findAll({
+            attributes: ['id', 'name', 'email', 'phone'],
+            include: [
+                {
+                    model: UsersTourneys,
+                    as: 'usersTourneys',
+                    where: tourneyId ? { tourneyId } : undefined,
+                    include: [
+                        {
+                            model: Tourney,
+                            as: 'tourney',
+                            attributes: ['id', 'name'],
+                        },
+                        {
+                            model: Team,
+                            as: 'team',
+                            attributes: ['id', 'teamName', 'type'],
+                        },
+                    ],
+                },
+                {
+                    model: Role,
+                    as: 'role',
+                    attributes: ['id', 'name'],
+                },
+            ],
+        });
+
+        res.json(users);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des utilisateurs avec détails :', error);
+        res.status(500).json({ message: 'Erreur serveur' });
+    }
+};
+
+exports.createUser = async (req, res) => {
+    // Vérifier que l'utilisateur est admin
+    if (req.user.roleId !== roles.ADMIN) {
+        return res.status(403).json({ message: 'Accès interdit.' });
+    }
+
+    const { name, email, password, phone, roleId, tourneyIds } = req.body;
+
+    try {
+        // Vérifier si l'email existe déjà
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Cet email est déjà utilisé.' });
+        }
+
+        // Hasher le mot de passe
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Créer l'utilisateur
+        const user = await User.create({ name, email, password: hashedPassword, phone, roleId });
+
+        // Assigner l'utilisateur aux tournois
+        if (Array.isArray(tourneyIds)) {
+            const usersTourneysData = tourneyIds.map((tourneyId) => ({
+                userId: user.id,
+                tourneyId,
+            }));
+            await UsersTourneys.bulkCreate(usersTourneysData);
+        }
+
+        // Exclure le mot de passe de la réponse
+        const userData = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            roleId: user.roleId,
+        };
+
+        res.status(201).json(userData);
+    } catch (error) {
+        console.error('Erreur lors de la création de l\'utilisateur :', error);
+        res.status(500).json({ message: 'Erreur serveur' });
+    }
+};
+
+exports.removeUserFromTourney = async (req, res) => {
+    const { userId, tourneyId } = req.params;
+
+    try {
+        // Vérifier que l'utilisateur est admin
+        if (req.user.roleId !== roles.ADMIN) {
+            return res.status(403).json({ message: 'Accès interdit.' });
+        }
+
+        const association = await UsersTourneys.findOne({
+            where: { userId, tourneyId },
+        });
+
+        if (!association) {
+            return res.status(404).json({ message: 'Association non trouvée.' });
+        }
+
+        await association.destroy();
+
+        res.status(200).json({ message: 'Utilisateur retiré du tournoi avec succès.' });
+    } catch (error) {
+        console.error('Erreur lors du retrait de l\'utilisateur du tournoi :', error);
+        res.status(500).json({ message: 'Erreur serveur' });
+    }
+};
+
 // Récupérer tous les tournois auxquels un utilisateur participe
 exports.getTourneysByUser = async (req, res) => {
     const { userId } = req.params;
 
     try {
+        // Vérifier si l'utilisateur fait la requête pour lui-même ou s'il est admin
+        if (req.user.id !== parseInt(userId, 10) && req.user.roleId !== roles.ADMIN) {
+            return res.status(403).json({ message: 'Accès interdit.' });
+        }
+
         const userTourneys = await UsersTourneys.findAll({
             where: { userId },
             include: [
@@ -129,22 +257,43 @@ exports.getUserById = async (req, res) => {
     }
 };
 
-// Mettre à jour un utilisateur (soi-même ou admin)
+// Mettre à jour un utilisateur (admin ou soi-même)
 exports.updateUser = async (req, res) => {
     const { userId } = req.params;
-    const { name, email, phone } = req.body;
+    const { name, email, phone, password } = req.body;
 
     try {
-        // Vérifier si l'utilisateur fait la requête pour lui-même ou s'il est admin
-        if (req.user.id !== parseInt(userId, 10) && req.user.roleId !== roles.ADMIN) {
-            return res.status(403).json({ message: 'Accès interdit. Vous ne pouvez pas modifier cet utilisateur.' });
-        }
+        // Vérification des permissions déjà gérée par authorizeUserOrAdmin
 
         const user = await User.findByPk(userId);
         if (!user) return res.status(404).json({ message: "Utilisateur non trouvé." });
 
+        const updatedData = { name, email, phone };
+
+        if (password) {
+            // Si l'utilisateur est un admin, il peut changer le mot de passe sans l'ancien mot de passe
+            if (req.user.roleId === roles.ADMIN) {
+                // Hasher le nouveau mot de passe
+                const hashedPassword = await bcrypt.hash(password, 10);
+                updatedData.password = hashedPassword;
+            } else {
+                // Si l'utilisateur n'est pas admin, il doit fournir l'ancien mot de passe
+                const { oldPassword } = req.body;
+                if (!oldPassword) {
+                    return res.status(400).json({ message: 'Ancien mot de passe requis pour changer le mot de passe.' });
+                }
+                const isMatch = await bcrypt.compare(oldPassword, user.password);
+                if (!isMatch) {
+                    return res.status(400).json({ message: 'Ancien mot de passe incorrect.' });
+                }
+                // Hasher le nouveau mot de passe
+                const hashedPassword = await bcrypt.hash(password, 10);
+                updatedData.password = hashedPassword;
+            }
+        }
+
         // Mettre à jour les informations de l'utilisateur
-        await user.update({ name, email, phone });
+        await user.update(updatedData);
 
         // Exclure le champ `password` dans la réponse
         const updatedUser = await User.findByPk(userId, {
@@ -153,9 +302,9 @@ exports.updateUser = async (req, res) => {
                 {
                     model: Role,
                     as: 'role',
-                    attributes: ['id', 'name']
-                }
-            ]
+                    attributes: ['id', 'name'],
+                },
+            ],
         });
 
         res.json(updatedUser);
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,19 @@
+// server/routes/user.js
 const express = require('express');
 const router = express.Router();
-const { getTourneysByUser, getAllUsers, getUserById, createUser, updateUser, deleteUser, getOwnProfile, } = require('../controllers/userController');
+const {
+    getTourneysByUser, getAllUsers, getUserById, createUser, updateUser, deleteUser, getOwnProfile, getAllUsersWithDetails, removeUserFromTourney,
+} = require('../controllers/userController');
 const { isAdmin, isAuthenticated, authorizeUserOrAdmin } = require('../middlewares');
 
-// Base URL: http://localhost:3000/api/users/:userId
-// 'CRUD' sans Create car géré dans la route auth.js
-router.get('/:userId/tourneys', isAuthenticated, authorizeUserOrAdmin, getTourneysByUser); // Récupérer les tournois pour un utilisateur donné
-router.get('/', isAuthenticated, isAdmin, getAllUsers);
-router.get('/:userId', isAuthenticated, authorizeUserOrAdmin, getUserById);
-router.put('/:userId', isAuthenticated, authorizeUserOrAdmin, updateUser);
-router.delete('/:userId', isAuthenticated, isAdmin, deleteUser);
+// Base URL: http://localhost:3000/api/users/
+router.get('/all/details', isAuthenticated, isAdmin, getAllUsersWithDetails); // Récupérer tous les utilisateurs avec leurs détails
+router.post('/', isAuthenticated, isAdmin, createUser); // Créer un utilisateur (admin seulement)
+router.delete('/:userId/tourneys/:tourneyId', isAuthenticated, isAdmin, removeUserFromTourney); // Supprimer un utilisateur d'un tournoi (admin seulement)
+router.get('/:userId/tourneys', isAuthenticated, authorizeUserOrAdmin, getTourneysByUser); // Récupérer les tournois pour un utilisateur donné (soi-même ou admin)
+router.get('/', isAuthenticated, isAdmin, getAllUsers); // Récupérer tous les utilisateurs (admin seulement)
+router.get('/:userId', isAuthenticated, authorizeUserOrAdmin, getUserById); // Récupérer un utilisateur par ID (soi-même ou admin)
+router.put('/:userId', isAuthenticated, authorizeUserOrAdmin, updateUser); // Mettre à jour un utilisateur (soi-même ou admin)
+router.delete('/:userId', isAuthenticated, isAdmin, deleteUser); // Supprimer un utilisateur (admin seulement)
 
 module.exports = router;
