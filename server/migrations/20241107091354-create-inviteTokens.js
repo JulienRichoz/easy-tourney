414f498ec64c9dcf2674b18d530f034a4a4ca6a9
Julien Richoz <julienrichoz@outlook.com>
2024-11-07 10:15:00 +0100

    Add migration inviteTokens

---
+++ server/migrations/20241107091354-create-inviteTokens.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('InviteTokens', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      tourneyId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Tourneys',
+          key: 'id',
+        },
+        onDelete: 'CASCADE', // Supprimer les tokens si le tournoi est supprimé
+      },
+      expiresAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      isValid: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('InviteTokens');
+  }
+};
