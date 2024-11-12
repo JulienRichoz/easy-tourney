ad396a48488552c39697badc55d0327b62392d90
Julien Richoz <julienrichoz@outlook.com>
2024-11-12 14:40:30 +0100

    Add migration new table poolSchedule

---
+++ server/migrations/20241112130348-create-poolSchedule.js
@@ -0,0 +1,55 @@
+// migrations/create-poolSchedule.js
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PoolSchedules', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      poolId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Pools',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      fieldId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Fields',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      startTime: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      endTime: {
+        type: Sequelize.TIME,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PoolSchedules');
+  },
+};
