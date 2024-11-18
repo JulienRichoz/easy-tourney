0c50bbb5738e32e27887a47083dc306614839c68
Julien Richoz <julienrichoz@outlook.com>
2024-11-18 13:11:56 +0100

    Update seeder to apply only for the first 2 id tourneys

--- server/seeders/20241011141536-demo-scheduleTourneys.js
+++ server/seeders/20241011141536-demo-scheduleTourneys.js
@@ -1,37 +1,40 @@
-// seeders/xxx_demo-scheduleTourneys.js
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // Récupérer les deux premiers tournois
     const tourneys = await queryInterface.sequelize.query(
-      'SELECT id FROM `Tourneys` WHERE name = "Tournoi de Printemps";',
+      'SELECT id FROM `Tourneys` ORDER BY id ASC LIMIT 2;',
       {
         type: Sequelize.QueryTypes.SELECT,
       }
     );
 
+    // Vérifier si au moins deux tournois existent
     if (tourneys.length > 0) {
-      await queryInterface.bulkInsert('ScheduleTourneys', [
-        {
-          tourneyId: tourneys[0].id,
-          startTime: '08:00',
-          endTime: '18:00',
-          introStart: '08:00',
-          introEnd: '08:30',
-          lunchStart: '12:00',
-          lunchEnd: '13:00',
-          outroStart: '17:30',
-          outroEnd: '18:00',
-          timeMatchRotation: 60,
-          timePoolRotation: 120,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ]);
+      const scheduleData = tourneys.map((tourney, index) => ({
+        tourneyId: tourney.id,
+        startTime: '08:00',
+        endTime: '18:00',
+        introStart: '08:00',
+        introEnd: '08:30',
+        lunchStart: '12:00',
+        lunchEnd: '13:00',
+        outroStart: '17:30',
+        outroEnd: '18:00',
+        timeMatchRotation: 60,
+        timePoolRotation: 120,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }));
+
+      // Insérer les données pour les deux tournois
+      await queryInterface.bulkInsert('ScheduleTourneys', scheduleData);
     }
   },
 
   down: async (queryInterface, Sequelize) => {
+    // Supprimer toutes les données insérées par ce seeder
     await queryInterface.bulkDelete('ScheduleTourneys', null, {});
   },
 };
