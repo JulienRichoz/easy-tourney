cc8d7b97108b97a6c6a8ae8d3a476729d74b6f0e
Julien Richoz <julienrichoz@outlook.com>
2024-11-30 00:47:46 +0100

    update userTourney store to handle new guard

--- frontend/src/store/modules/userTourney.js
+++ frontend/src/store/modules/userTourney.js
@@ -5,17 +5,35 @@ export default {
   namespaced: true,
   state: {
     activeTourney: null,
+    selectedTourney: {}, // Stocker les données d'un tournoi spécifique
     tourneyRole: null,
   },
   mutations: {
     SET_ACTIVE_TOURNEY(state, tourney) {
       state.activeTourney = tourney;
     },
+    SET_SELECTED_TOURNEY(state, tourney) {
+      state.selectedTourney[tourney.id] = tourney; // Stocke les données par ID
+    },
     SET_TOURNEY_ROLE(state, role) {
       state.tourneyRole = role;
     },
   },
   actions: {
+    async fetchTourneyById({ state, commit }, tourneyId) {
+      if (state.selectedTourney[tourneyId]) {
+        return state.selectedTourney[tourneyId]; // Retourne les données si déjà chargées
+      }
+      try {
+        const response = await apiService.get(`/tourneys/${tourneyId}`);
+        const tourneyData = response.data;
+        commit('SET_SELECTED_TOURNEY', tourneyData);
+        return tourneyData;
+      } catch (error) {
+        console.error('Erreur lors de la récupération du tournoi :', error);
+        throw error;
+      }
+    },
     async fetchActiveTourney({ commit, dispatch }) {
       try {
         const response = await apiService.get('/users/active-tourney');
@@ -29,7 +47,6 @@ export default {
             await dispatch('fetchTourneyRole', tourneyId);
           }
         } else {
-          // Si aucun tournoi actif, on peut gérer cela ici
           commit('SET_ACTIVE_TOURNEY', null);
           commit('SET_TOURNEY_ROLE', null);
         }
@@ -45,7 +62,6 @@ export default {
         const response = await apiService.get(
           `/users/me/tourneys/${tourneyId}/role`
         );
-
         commit('SET_TOURNEY_ROLE', response.data.tourneyRole);
       } catch (error) {
         console.error(
@@ -58,6 +74,9 @@ export default {
   getters: {
     isAssistant(state) {
       return state.tourneyRole === 'assistant';
+    },
+    getTourneyById: (state) => (id) => {
+      return state.selectedTourney[id];
     },
   },
 };
