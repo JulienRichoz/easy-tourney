d22f67f1178713b71018eff59c0a2feb0e92f535
Julien Richoz <julienrichoz@outlook.com>
2024-11-29 17:59:38 +0100

    Add default test to check if store getter is working

--- frontend/src/views/user/tourneys/TourneyGameDetails.vue
+++ frontend/src/views/user/tourneys/TourneyGameDetails.vue
@@ -1,6 +1,34 @@
 <!-- src/views/user/tourneys/TourneyGameDetails.vue -->
 <template>
   <div>
-    <div class="p-6">En développement</div>
+    <!-- Affichage des détails du match -->
+    <div>
+      <!-- Détails du match -->
+    </div>
+
+    <!-- Section de mise à jour des scores (assistants seulement) -->
+    <div v-if="$store.getters['user/isAssistant']">
+      Bonjour assistant
+      <!-- Formulaire de mise à jour des scores -->
+    </div>
+    <div v-else>
+      <p>Bonjour</p>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {};
+    },
+    computed: {
+      isAssistant() {
+        return this.$store.state.user.user.roleId === 'assistant'; // Adaptez selon votre structure
+      },
+    },
+    methods: {
+      // ... vos méthodes
+    },
+  };
+</script>
