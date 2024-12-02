2add8e7aceaa8f0e98ad6ffd4352492b0c13b961
Julien Richoz <julienrichoz@outlook.com>
2024-12-02 23:56:51 +0100

    Add component collapse box

---
+++ frontend/src/components/user/CollapsibleBox.vue
@@ -0,0 +1,32 @@
+<!-- CollapsibleBox.vue -->
+<template>
+  <div class="mb-4">
+    <button
+      @click="isOpen = !isOpen"
+      class="w-full flex justify-between items-center bg-blue-500 text-white px-4 py-2 rounded"
+    >
+      <span>{{ title }}</span>
+      <span v-if="isOpen">-</span>
+      <span v-else>+</span>
+    </button>
+    <div v-show="isOpen" class="mt-2 p-4 border rounded">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        isOpen: true,
+      };
+    },
+  };
+</script>
