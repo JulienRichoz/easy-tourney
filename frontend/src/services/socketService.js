f79f265b4a617c13c1e14b54a33e448acbd14a6d
Julien Richoz <julienrichoz@outlook.com>
2024-12-03 12:54:04 +0100

    Add client service websocket

---
+++ frontend/src/services/socketService.js
@@ -0,0 +1,33 @@
+// src/services/socketService.js
+
+import { io } from 'socket.io-client';
+
+let socket = null;
+
+export function getSocket() {
+    if (!socket) {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            // Ne pas créer de socket si l'utilisateur n'est pas authentifié
+            return null;
+        }
+        socket = io(process.env.VUE_APP_IMAGE_URL || 'http://localhost:3000', {
+            auth: {
+                token,
+            },
+            reconnectionAttempts: 5,
+            reconnectionDelay: 5000,
+        });
+
+        // Gestion des erreurs et des événements
+        socket.on('connect_error', (err) => {
+            console.error('Erreur de connexion Socket.IO :', err.message);
+        });
+
+        socket.on('disconnect', (reason) => {
+            console.warn('Socket déconnecté :', reason);
+            // Si nécessaire, gérer la reconnexion
+        });
+    }
+    return socket;
+}
