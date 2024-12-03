980a22b2283394af584cfb5497b2ebe03e2e8cc8
Julien Richoz <julienrichoz@outlook.com>
2024-12-03 15:54:29 +0100

    Add correct middleware to check if assistant or admin backend

--- server/routes/gameEvent.js
+++ server/routes/gameEvent.js
@@ -1,9 +1,11 @@
 // server/routes/gameEvent.js
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+const tourneyRoles = require('../config/tourneyRoles');
+
 const {
   createGameEvent,
-  getGameEventsByGame,
+  getGameEvents,
   updateGameEvent,
   deleteGameEvent,
 } = require('../controllers/gameEventController');
@@ -12,10 +14,12 @@ const {
   authorizeTournamentAccess,
 } = require('../middlewares');
 
+const { authorizeTourneyRoles } = require('../middlewares/authorizeTourneyRole');
+
 // Base URL: /api/tourneys/:tourneyId/games/:gameId/events
 
 router.post('/', isAuthenticated, createGameEvent); // Créer un événement pour un match
-router.get('/', isAuthenticated, authorizeTournamentAccess, getGameEventsByGame); // Récupérer les événements d'un match
+router.get('/', isAuthenticated, authorizeTournamentAccess, authorizeTourneyRoles([tourneyRoles.ASSISTANT]), getGameEvents); // Récupérer les événements d'un match
 router.put('/:eventId', isAuthenticated, updateGameEvent); // Mettre à jour un événement
 router.delete('/:eventId', isAuthenticated, deleteGameEvent); // Supprimer un événement
 
