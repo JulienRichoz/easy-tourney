f9a11ac7be6f51c7d4c0bb9411c43d0f1575386f
Julien Richoz <julienrichoz@outlook.com>
2024-12-10 15:14:31 +0100

    Add middleware to control user role

--- server/routes/gameEvent.js
+++ server/routes/gameEvent.js
@@ -18,9 +18,9 @@ const { authorizeTourneyRoles } = require('../middlewares/authorizeTourneyRole')
 
 // Base URL: /api/tourneys/:tourneyId/games/:gameId/events
 
-router.post('/', isAuthenticated, createGameEvent); // Créer un événement pour un match
+router.post('/', isAuthenticated, authorizeTournamentAccess, authorizeTourneyRoles([tourneyRoles.ASSISTANT]), createGameEvent); // Créer un événement pour un match
 router.get('/', isAuthenticated, authorizeTournamentAccess, getGameEvents); // Récupérer les événements d'un match
-router.put('/:eventId', isAuthenticated, updateGameEvent); // Mettre à jour un événement
-router.delete('/:eventId', isAuthenticated, deleteGameEvent); // Supprimer un événement
+router.put('/:eventId', isAuthenticated, authorizeTournamentAccess, authorizeTourneyRoles([tourneyRoles.ASSISTANT]), updateGameEvent); // Mettre à jour un événement
+router.delete('/:eventId', isAuthenticated, authorizeTournamentAccess, authorizeTourneyRoles([tourneyRoles.ASSISTANT]), deleteGameEvent); // Supprimer un événement
 
 module.exports = router;
