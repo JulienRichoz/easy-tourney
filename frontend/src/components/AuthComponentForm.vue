c9fb3a7ba25c26395df2f0440eebd492790bd997
Julien Richoz <julienrichoz@outlook.com>
2024-12-18 00:35:19 +0100

    Refactor AuthComponent to handle forgot password and reset password

--- frontend/src/components/AuthComponentForm.vue
+++ frontend/src/components/AuthComponentForm.vue
@@ -8,7 +8,7 @@
       <h1
         class="text-2xl font-bold text-center mb-8 text-light-title dark:text-dark-title"
       >
-        {{ mode === 'login' ? 'Connexion' : 'Inscription' }}
+        {{ title }}
       </h1>
       <form @submit.prevent="handleSubmit">
         <!-- Champ Nom (uniquement pour l'inscription) -->
@@ -21,6 +21,7 @@
           </label>
           <input
             id="name"
+            name="name"
             v-model="formData.name"
             type="text"
             placeholder="Entrer votre nom"
@@ -41,7 +42,7 @@
         </div>
 
         <!-- Champ Email -->
-        <div class="mb-6">
+        <div v-if="showEmailField" class="mb-6">
           <label
             for="email"
             class="block text-light-form-text dark:text-dark-form-text font-semibold mb-2"
@@ -50,9 +51,11 @@
           </label>
           <input
             id="email"
+            name="email"
             v-model="formData.email"
             type="email"
             placeholder="Entrer votre email"
+            autocomplete="email"
             class="w-full p-3 border rounded-md bg-light-form-background dark:bg-dark-form-background text-light-form-text dark:text-dark-form-text"
             :class="{
               'border-light-form-error dark:border-dark-form-error':
@@ -70,7 +73,7 @@
         </div>
 
         <!-- Champ Mot de Passe -->
-        <div class="mb-6">
+        <div v-if="showPasswordFields" class="mb-6">
           <label
             for="password"
             class="block text-light-form-text dark:text-dark-form-text font-semibold mb-2"
@@ -79,9 +82,11 @@
           </label>
           <input
             id="password"
+            name="password"
             v-model="formData.password"
             type="password"
             placeholder="Entrer votre mot de passe"
+            autocomplete="new-password"
             class="w-full p-3 border rounded-md bg-light-form-background dark:bg-dark-form-background text-light-form-text dark:text-dark-form-text"
             :class="{
               'border-light-form-error dark:border-dark-form-error':
@@ -98,18 +103,44 @@
           </p>
         </div>
 
+        <!-- Champ Confirmation Mot de Passe -->
+        <div v-if="showConfirmPasswordField" class="mb-6">
+          <label
+            for="confirmPassword"
+            class="block text-light-form-text dark:text-dark-form-text font-semibold mb-2"
+          >
+            Confirmer le mot de passe
+          </label>
+          <input
+            id="confirmPassword"
+            name="confirmPassword"
+            v-model="formData.confirmPassword"
+            type="password"
+            placeholder="Confirmer votre mot de passe"
+            autocomplete="new-password"
+            class="w-full p-3 border rounded-md bg-light-form-background dark:bg-dark-form-background text-light-form-text dark:text-dark-form-text"
+            :class="{
+              'border-light-form-error dark:border-dark-form-error':
+                errors.confirmPassword,
+              'border-light-form-border-default dark:border-dark-form-border-default':
+                !errors.confirmPassword,
+            }"
+          />
+          <p v-if="errors.confirmPassword" class="text-red-500 text-sm mt-1">
+            {{ errors.confirmPassword }}
+          </p>
+        </div>
+
         <!-- Bouton de soumission -->
         <ButtonComponent
           variant="primary"
           nativeType="submit"
-          class="w-full"
+          class="w-full ml0 mr-0 sm:ml-0 sm:mr-0 px-2 sm:px-4 py-2"
           :disabled="isSubmitting"
         >
-          {{ mode === 'login' ? 'Se connecter' : "S'inscrire" }}
+          {{ submitButtonText }}
         </ButtonComponent>
       </form>
-
-      <!-- Message d'erreur général -->
       <p
         v-if="error"
         class="text-light-form-error dark:text-dark-form-error mt-4 text-center font-semibold"
@@ -117,29 +148,50 @@
         {{ error }}
       </p>
 
-      <!-- Lien vers l'inscription ou la connexion -->
-      <p
-        v-if="mode === 'login'"
+      <!-- Liens contextuels -->
+      <div
         class="text-center mt-6 text-light-form-text dark:text-dark-form-text"
       >
-        Pas de compte ?
-        <router-link
-          to="/register"
-          class="text-light-menuHoverText dark:text-dark-menuHoverText font-semibold"
-          >Créer un compte</router-link
-        >
-      </p>
-      <p
-        v-else
-        class="text-center mt-6 text-light-form-text dark:text-dark-form-text"
-      >
-        Déjà un compte ?
-        <router-link
-          to="/login"
-          class="text-light-menuHoverText dark:text-dark-menuHoverText font-semibold"
-          >Se connecter</router-link
-        >
-      </p>
+        <template v-if="mode === 'login'">
+          Pas de compte ?
+          <router-link
+            to="/register"
+            class="text-light-menuHoverText dark:text-dark-menuHoverText font-semibold"
+            >Créer un compte</router-link
+          >
+          <br />
+          <router-link
+            to="/forgot-password"
+            class="text-light-menuHoverText dark:text-dark-menuHoverText font-semibold block"
+            >Mot de passe oublié ?</router-link
+          >
+        </template>
+
+        <template v-else-if="mode === 'register'">
+          Déjà un compte ?
+          <router-link
+            to="/login"
+            class="text-light-menuHoverText dark:text-dark-menuHoverText font-semibold"
+            >Se connecter</router-link
+          >
+        </template>
+
+        <template v-else-if="mode === 'forgot-password'">
+          <router-link
+            to="/login"
+            class="text-light-menuHoverText dark:text-dark-menuHoverText font-semibold"
+            >Retour à la connexion</router-link
+          >
+        </template>
+
+        <template v-else-if="mode === 'reset-password'">
+          <router-link
+            to="/login"
+            class="text-light-menuHoverText dark:text-dark-menuHoverText font-semibold"
+            >Retour à la connexion</router-link
+          >
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -155,7 +207,7 @@
     props: {
       mode: {
         type: String,
-        default: 'login', // 'login' ou 'register'
+        default: 'login', // 'login', 'register', 'forgot-password', 'reset-password'
       },
       error: {
         type: String,
@@ -173,34 +225,154 @@
           name: '',
           email: '',
           password: '',
+          confirmPassword: '',
         },
         errors: {},
       };
+    },
+    computed: {
+      showEmailField() {
+        return this.mode !== 'reset-password';
+      },
+      showPasswordFields() {
+        return (
+          this.mode === 'login' ||
+          this.mode === 'register' ||
+          this.mode === 'reset-password'
+        );
+      },
+      showConfirmPasswordField() {
+        return this.mode === 'register' || this.mode === 'reset-password';
+      },
+      title() {
+        switch (this.mode) {
+          case 'login':
+            return 'Connexion';
+          case 'register':
+            return 'Inscription';
+          case 'forgot-password':
+            return 'Mot de passe oublié';
+          case 'reset-password':
+            return 'Réinitialiser le mot de passe';
+          default:
+            return 'Authentification';
+        }
+      },
+      submitButtonText() {
+        switch (this.mode) {
+          case 'login':
+            return 'Se connecter';
+          case 'register':
+            return "S'inscrire";
+          case 'forgot-password':
+            return 'Envoyer le lien';
+          case 'reset-password':
+            return 'Réinitialiser le mot de passe';
+          default:
+            return 'Soumettre';
+        }
+      },
     },
     methods: {
       handleSubmit() {
         this.errors = {};
 
-        // Validation basique
-        if (this.mode === 'register' && !this.formData.name) {
+        // Validation en fonction du mode
+        if (this.mode === 'register') {
+          this.validateRegister();
+        } else if (this.mode === 'login') {
+          this.validateLogin();
+        } else if (this.mode === 'forgot-password') {
+          this.validateForgotPassword();
+        } else if (this.mode === 'reset-password') {
+          this.validateResetPassword();
+        }
+
+        if (Object.keys(this.errors).length === 0) {
+          // Émission des données appropriées en fonction du mode
+          let payload = {};
+          if (this.mode === 'login' || this.mode === 'register') {
+            payload = { ...this.formData };
+          } else if (this.mode === 'forgot-password') {
+            payload = { email: this.formData.email };
+          } else if (this.mode === 'reset-password') {
+            payload = {
+              password: this.formData.password,
+              confirmPassword: this.formData.confirmPassword,
+            };
+          }
+
+          this.$emit('submit', payload);
+        }
+      },
+      validateRegister() {
+        if (!this.formData.name) {
           this.errors.name = 'Le nom est obligatoire.';
         }
+
         if (!this.formData.email) {
           this.errors.email = "L'email est obligatoire.";
         } else if (!this.validateEmail(this.formData.email)) {
           this.errors.email = 'Veuillez entrer un email valide.';
         }
+
+        if (!this.formData.password) {
+          this.errors.password = 'Le mot de passe est obligatoire.';
+        } else {
+          const passwordRegex = /^(?=.*[A-Z]).{8,}$/;
+          if (!passwordRegex.test(this.formData.password)) {
+            this.errors.password =
+              'Le mot de passe doit contenir au moins 8 caractères et une majuscule.';
+          }
+        }
+
+        if (!this.formData.confirmPassword) {
+          this.errors.confirmPassword =
+            'Veuillez confirmer votre mot de passe.';
+        } else if (this.formData.password !== this.formData.confirmPassword) {
+          this.errors.confirmPassword =
+            'Les mots de passe ne correspondent pas.';
+        }
+      },
+      validateLogin() {
+        if (!this.formData.email) {
+          this.errors.email = "L'email est obligatoire.";
+        } else if (!this.validateEmail(this.formData.email)) {
+          this.errors.email = 'Veuillez entrer un email valide.';
+        }
+
         if (!this.formData.password) {
           this.errors.password = 'Le mot de passe est obligatoire.';
         }
+      },
+      validateForgotPassword() {
+        if (!this.formData.email) {
+          this.errors.email = "L'email est obligatoire.";
+        } else if (!this.validateEmail(this.formData.email)) {
+          this.errors.email = 'Veuillez entrer un email valide.';
+        }
+      },
+      validateResetPassword() {
+        if (!this.formData.password) {
+          this.errors.password = 'Le mot de passe est obligatoire.';
+        } else {
+          const passwordRegex = /^(?=.*[A-Z]).{8,}$/;
+          if (!passwordRegex.test(this.formData.password)) {
+            this.errors.password =
+              'Le mot de passe doit contenir au moins 8 caractères et une majuscule.';
+          }
+        }
 
-        if (Object.keys(this.errors).length === 0) {
-          // Pas d'erreurs, émettre l'événement 'submit' avec les données du formulaire
-          this.$emit('submit', { ...this.formData });
+        if (!this.formData.confirmPassword) {
+          this.errors.confirmPassword =
+            'Veuillez confirmer votre mot de passe.';
+        } else if (this.formData.password !== this.formData.confirmPassword) {
+          this.errors.confirmPassword =
+            'Les mots de passe ne correspondent pas.';
         }
       },
       validateEmail(email) {
-        // Simple regex pour valider l'email
+        // Regex simple pour valider l'email
         const re = /\S+@\S+\.\S+/;
         return re.test(email);
       },
@@ -212,6 +384,7 @@
           name: '',
           email: '',
           password: '',
+          confirmPassword: '',
         };
         this.errors = {};
       },
