58b285a5e71cd71f07b334335b7950de83e2f760
Julien Richoz <julienrichoz@outlook.com>
2024-12-18 23:02:28 +0100

    Remove console.log

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:3000/api';
 export const BASE_URL = process.env.VUE_APP_BASE_URL || 'http://localhost:8080';
-console.log(process.env);
 
 const apiService = axios.create({
   baseURL: API_URL,
