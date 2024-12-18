2944256ef53aac2fc68688de26de0d31a1601dab
Julien Richoz <julienrichoz@outlook.com>
2024-12-18 17:52:13 +0100

    Remove image too heavy

--- frontend/src/views/shared/AccessDenied.vue
+++ frontend/src/views/shared/AccessDenied.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="flex h-dvh">
-    <!-- Partie gauche avec une image -->
-    <div class="w-1/2 bg-gray-100 flex justify-center items-center">
-      <img
-        src="@/assets/403_accessDenied.png"
-        alt="Access Denied Image"
-        class="w-full h-full object-cover"
-      />
-    </div>
-
-    <!-- Partie droite avec du texte stylé -->
-    <div class="w-1/2 flex flex-col justify-center items-center bg-gray-700">
+  <div class="flex h-dvh bg-gray-100">
+    <!-- Partie gauche avec un texte -->
+    <div class="w-1/2 flex flex-col justify-center items-center">
       <h1 class="text-6xl font-extrabold text-green-400 mb-6">
         403 Access Denied
       </h1>
-      <p class="text-2xl text-white text-center px-8">
+      <p class="text-2xl text-gray-700 text-center px-8">
         Sorry, you don’t have the necessary permissions to view this page.
+      </p>
+    </div>
+
+    <!-- Partie droite avec un style similaire -->
+    <div class="w-1/2 flex justify-center items-center bg-gray-700">
+      <p class="text-4xl text-white text-center">
+        Please contact your administrator if you think this is a mistake.
       </p>
     </div>
   </div>
--- frontend/src/views/shared/NotFound.vue
+++ frontend/src/views/shared/NotFound.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="flex h-screen">
-    <!-- Partie gauche avec fond personnalisé (potentiellement pour une image) -->
-    <div class="w-1/2 bg-gray-100 flex justify-center items-center">
-      <img
-        src="@/assets/404_notFound.png"
-        alt="Access Denied Image"
-        class="w-full h-full object-cover"
-      />
+  <div class="flex h-dvh bg-gray-100">
+    <!-- Partie gauche avec un texte -->
+    <div class="w-1/2 flex flex-col justify-center items-center">
+      <h1 class="text-6xl font-extrabold text-green-400 mb-6">404 Not Found</h1>
+      <p class="text-2xl text-gray-700 text-center px-8">
+        Oops! The page you're looking for doesn't exist.
+      </p>
     </div>
 
-    <!-- Partie droite avec du texte stylé -->
-    <div class="w-1/2 flex flex-col justify-center items-center bg-gray-700">
-      <h1 class="text-6xl font-extrabold text-green-400 mb-6">404 Not Found</h1>
-      <p class="text-2xl text-white text-center px-8">
-        Oops! The page you're looking for doesn't exist.
+    <!-- Partie droite avec un style similaire -->
+    <div class="w-1/2 flex justify-center items-center bg-gray-700">
+      <p class="text-4xl text-white text-center">
+        Try returning to the homepage or check the URL.
       </p>
     </div>
   </div>
@@ -25,6 +23,4 @@
   };
 </script>
 
-<style scoped>
-  /* Pas de style supplémentaire, tout est géré par Tailwind */
-</style>
+<style scoped></style>
